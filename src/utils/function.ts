95c0150f88f4a6c868b95f793ed6624de581cf85
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2024-09-27 10:59:15 +0700

    Update

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,7 +1,7 @@
 export class Func {
 
   static downloadCv() {
-    window.open("https://drive.google.com/file/d/1tdKQNNXXt3sM6lL2cBdWYJBTi2h_dq2G/view?usp=drive_link", "_blank");
+    window.open("https://drive.google.com/file/d/1_YKQVOSaiB93KEQuAIDIpQm6P6iD3t0S/view?usp=drive_link", "_blank");
   }
 
 }

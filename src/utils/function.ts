b12d3ece3b9dae909669817d63ee9e3202b7762e
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2025-06-06 21:31:38 +0700

    update cv

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,7 +1,8 @@
 export class Func {
-
   static downloadCv() {
-    window.open("https://drive.google.com/file/d/1_YKQVOSaiB93KEQuAIDIpQm6P6iD3t0S/view?usp=drive_link", "_blank");
+    window.open(
+      "https://drive.google.com/file/d/1xkOip6HjptzdzK61YAJTwnJVnK-RGNsx/view?usp=drive_link",
+      "_blank"
+    );
   }
-
 }
